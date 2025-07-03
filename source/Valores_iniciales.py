eaf67fdf9988816846e27315888960aa675f9fb9
Jose López <142743138+ItzThrim@users.noreply.github.com>
2025-07-03 14:11:35 -0500

    Update Valores_iniciales.py

--- source/Valores_iniciales.py
+++ source/Valores_iniciales.py
@@ -1,16 +1,11 @@
 import numpy as np
 
-# Constantes globales
-M = np.array([
-    [0, 0], [1, 0], [0, 1], [-1, 0], [0, -1],
-    [1, 1], [-1, 1], [-1, -1], [1, -1]
-])
-pesos = np.array([4/9, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36, 1/9, 1/36])
-w = 2  # Parámetro de relajación
-
 def inicializar_red(Nx, Ny, vx_inicial=0.1):
+
     mat_vel = np.zeros((Nx, Ny, 2))
     mat_den = np.ones((Nx, Ny))
     f = np.zeros((Nx, Ny, 9))
+
     mat_vel[:, :, 0] = vx_inicial
+
     return mat_vel, mat_den, f
