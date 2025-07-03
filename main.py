1a57d8766b59ea33cb426500142b6c8a53654533
unknown <jolopezhe@unal.edu.co>
2025-07-03 14:18:45 -0500

    Anado funciones del algoritmo de Boltzmann y la funcion main

---
+++ main.py
@@ -0,0 +1,19 @@
+import numpy as np
+from Valores_iniciales import inicializar_red
+from Construccion_algoritmo import actualizar_equilibrio_global, colisionar, propagar, recalcular_macros
+
+Nx, Ny = 3, 3
+mat_vel, mat_den, f = inicializar_red(Nx, Ny)
+
+f = actualizar_equilibrio_global(f, mat_den, mat_vel)
+
+fcol = colisionar(f, w=2)
+
+fprop = propagar(fcol)
+
+mat_den, mat_vel = recalcular_macros(fprop)
+
+f = actualizar_equilibrio_global(f, mat_den, mat_vel)
+
+print("Densidad en (0,0):", mat_den[0, 0])
+print("Velocidad en (0,0):", mat_vel[0, 0])
