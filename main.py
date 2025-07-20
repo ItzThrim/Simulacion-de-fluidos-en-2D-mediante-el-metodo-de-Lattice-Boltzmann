054c22e24fe29023238f9da99cb9103e66344138
unknown <jolopezhe@unal.edu.co>
2025-07-20 11:44:34 -0500

    baca

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import imageio
 sys.path.append(os.path.join(os.path.dirname(__file__), 'source'))
 from source.parametros import *
 from source.algoritmo import *
-from source.obstaculo import crear_obstaculo_triangulo
+from source.obstaculo import crear_obstaculo_triangulo, crear_obstaculo_circulo, crear_obstaculo_rectangulo, crear_obstaculo_vaca
 from source.guardarimg import guardar
 from gif import crear_gif
 
@@ -22,7 +22,11 @@ imagenes = []
 
 f, vx, vy = flujo(Nx,Ny,vx,vy,M,f,u0,pesos,rho)
 
-Obs = crear_obstaculo_triangulo(10, 5, 20, 5, 15, 15)
+#Obs = crear_obstaculo_circulo(Nx//2, Ny//2, 20)
+#Obs = crear_obstaculo_triangulo(10, 10, 20, 30, 30, 10)
+#Obs = crear_obstaculo_rectangulo(10, 10, 20, 30)
+Obs = crear_obstaculo_vaca(int(Nx/2), int(Ny/2))
+
 
 for t in range(paso_de_tiempo):
     vx, vy, rho = colision(Nx, Ny, rho, vx, vy, Obs,f)
@@ -38,4 +42,4 @@ for t in range(paso_de_tiempo):
     if t % guardar_cada == 0:
         guardar(vx, vy, Obs, t,imagenes,ruta_imagenes)
 
-crear_gif(ruta_imagenes, "simulacion_lbm.gif")
+crear_gif(ruta_imagenes, "ejemplo_vaca_lbm.gif")
--- source/obstaculo.py
+++ source/obstaculo.py
@@ -23,3 +23,44 @@ def crear_obstaculo_circulo(xc, yc, radio):
             if (i - xc)**2 + (j - yc)**2 <= radio**2:
                 Obs[i, j] = True
     return Obs
+
+def crear_obstaculo_rectangulo(x1, y1, x2, y2):
+    Obs = np.zeros((Nx, Ny), dtype=bool)
+    for i in range(Nx):
+        for j in range(Ny):
+            if x1 <= i <= x2 and y1 <= j <= y2:
+                Obs[i, j] = True
+    return Obs
+
+def crear_obstaculo_vaca(x, y):
+    Obs = np.zeros((Nx, Ny), dtype=bool)
+
+    # Centro de la figura
+    xc, yc = x // 2, y // 2
+
+    # 1. Cuerpo (elipse grande)
+    a_body, b_body = 12, 7
+    for i in range(x):
+        for j in range(y):
+            xo, yo = i - xc, j - yc
+            if (xo**2 / a_body**2 + yo**2 / b_body**2) <= 1:
+                Obs[i, j] = True
+
+    # 2. Cabeza (elipse más pequeña)
+    a_head, b_head = 7, 4
+    head_center = (xc + a_body - 25, yc + 8)
+    for i in range(x):
+        for j in range(y):
+            xo, yo = i - head_center[0], j - head_center[1]
+            if (xo**2 / a_head**2 + yo**2 / b_head**2) <= 1:
+                Obs[i, j] = True
+
+    # 3. Patas (rectángulos)
+    leg_width, leg_height = 4, 10
+    for leg_x in [xc - 7, xc + 9]:
+        for i in range(leg_x - leg_width // 2, leg_x + leg_width // 2):
+            for j in range(yc - b_body, yc - b_body - leg_height, -1):
+                if 0 <= i < x and 0 <= j < y:
+                    Obs[i, j] = True
+
+    return Obs
