8738a0e3d4946843f273441d40ac64a13ea9e458
unknown <jolopezhe@unal.edu.co>
2025-07-21 18:15:37 -0500

    Comento la funcion para crear las imagenes.

--- source/guardarimg.py
+++ source/guardarimg.py
@@ -3,7 +3,11 @@ import imageio
 import numpy as np
 import os
 
-def guardar(ux, uy, obj, t, imagenes, carpeta):
+def guardar(ux, uy, obj, t, imagenes, carpeta, imagenes_densidad, drho, carpeta_den):
+    """
+    Guarda las imágenes de velocidad y densidad en las carpetas
+    
+    """
     vel = np.sqrt(ux**2 + uy**2)
     vel[obj] = 0
     plt.figure(figsize=(10, 4))
@@ -13,6 +17,17 @@ def guardar(ux, uy, obj, t, imagenes, carpeta):
     nombre = os.path.join(carpeta, f"imagen_{t:04d}.png")
     plt.savefig(nombre)
     plt.close()
-    imagenes.append(imageio.imread(nombre))
-    print(f"Paso {t} guardado")
+    imagenes.append(imageio.imread(nombre))       
+        
+    # Aca soloc opie y pegue lo de arriba no me reganen
+    plt.figure(figsize=(10, 4))
+    plt.imshow(drho.T, origin="lower", cmap="viridis")
+    plt.colorbar(label="Densidad")
+    plt.title(f"Densidad - Paso {t}")
+    nombre_rho = os.path.join(carpeta_den, f"imagen_{t:04d}.png")
+    plt.savefig(nombre_rho)
+    plt.close()
+    imagenes_densidad.append(imageio.imread(nombre_rho))
 
+    
+    print(f"Paso {t} guardado")
